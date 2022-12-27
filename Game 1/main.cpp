5689d2c487cec022b99dc28ecf0dc0b58bb67d7f
Cole Swanson <cole9swanson@gmail.com>
2022-12-26 20:28:56 -0600

    Start of fishing game

--- Game 1/main.cpp
+++ Game 1/main.cpp
@@ -24,75 +24,86 @@
  *              speed - speed of the fish (0 - 100)
  *              rarity of the fish ( Trash - 100-200 Common 200-500 Uncommon 500-750 Rare - 750-1000 legendary 1000-2000)
  *          derived from fish object:
- *          =====TRASH=====
+ *          =====TRASH=====	-	400/1000 (100)
  *          Old boot
  *              Speed: 0
  *              Rarity: 100
  *          Log
  *              Speed: 0
- *              Rarity: 150
- *          =====COMMON======
+ *              Rarity: 100
+ *	    Trash
+ *		Speed: 0
+ *		Rartiy: 100
+ *	    Snag
+ *		Speed: 0
+ *		Rarity: 100
+ *          =====COMMON======	-	300/1000 (50)
  *          Carp
  *              Speed: 15
- *              Rarity: 200
+ *              Rarity: 50
  *          Catfish
  *              Speed: 15
- *              Rarity: 250
+ *              Rarity: 50
  *          Goldfish
  *              Speed: 20
- *              Rarity: 250
+ *              Rarity: 50
  *          Nemo
  *              Speed: 25
- *              Rarity: 250
+ *              Rarity: 50
  *          Walleye
  *              Speed: 25
- *              Rarity: 400
+ *              Rarity: 50
  *          Perch
  *              Speed: 25
- *              Rarity: 400
- *          ======UNCOMMON======
+ *              Rarity: 50
+ *          ======UNCOMMON====== -	150/1000(30)
  *          Frog
  *              Speed: (random speed changes every 2 seconds)
- *              Rarity: 500
+ *              Rarity: 30
  *          Duck
  *              Speed: 50
- *              Rarity: 550
+ *              Rarity: 30
  *          Turtle
  *              Speed: 5
- *              Rarity: 600
+ *              Rarity: 30
  *          Jellyfish
  *              Speed: 30
- *              Rarity: 650
+ *              Rarity: 30
  *          Seahorse
  *              Speed: 40
- *              Rarity: 700
+ *              Rarity: 30
  *          Lobster
  *              Speed: 5
- *              Rarity: 700
- *          =====RARE=====
+ *              Rarity: 30
+ *          =====RARE=====	-	100/1000(25)
  *          Sword Fish
  *              Speed: 75
- *              Rarity: 800
+ *              Rarity: 25
  *          Octopus
  *              Speed: 65
- *              Rarity: 850
+ *              Rarity: 25
  *          Whale
  *              Speed: 70
- *              Rarity: 900
+ *              Rarity: 25
  *          Sting ray
  *              Speed: 75
- *              Rarity: 1000
- *          ======LEGENDARY======
+ *              Rarity: 25
+ *          ======LEGENDARY====== -	50/1000(10)
  *         Starfish
- *              Speed: 0
- *              Rarity: 1500
+ *              Speed: 5
+ *              Rarity: 10
  *         Shark
  *              Speed: 100
- *              Rarity: 1750
+ *              Rarity: 10
  *         Penguin
  *              Speed: 90
- *              Rarity: 2000
- *
+ *              Rarity: 10
+ *	       Treasure Chest
+ *		        Speed: 0
+ *		        Rarity: 10
+ *	       Blob fish
+ *		        Speed: 80
+ *		        Rarity: 10
  */
 
 #include <SFML/Graphics.hpp>
@@ -101,70 +112,44 @@
 
 using namespace sf;
 
-//Fishing rod
-class FishingRod
+//Fishing rod object
+class Entity
 {
 public:
-    //Constructor
-    FishingRod(const std::string &name, float x, float y, float dx, float dy, int r, float angle, bool cast, bool row) {
-        name = "FishingRod";
-        x = 100;
-        y = 100;
-        dx = 0;
-        dy = 0;
-        R = 50;
-        angle = 0;
-        cast = false;
-        row = false;
-    }
     //getter and setter for X coordinate
     float getX() const {
         return x;
     }
     void setX(float x) {
-        FishingRod::x = x;
+        Entity::x = x;
     }
     //getter and setter for y coordinate
     float getY() const {
         return y;
     }
     void setY(float y) {
-        FishingRod::y = y;
+        Entity::y = y;
     }
     //getter and setter for dx
     float getDx() const {
         return dx;
     }
     void setDx(float dx) {
-        FishingRod::dx = dx;
+        Entity::dx = dx;
     }
     //getter and setter for dy
     float getDy() const {
         return dy;
     }
     void setDy(float dy) {
-        FishingRod::dy = dy;
+        Entity::dy = dy;
     }
     //getter and setter for angle
     float getAngle() const {
         return angle;
     }
     void setAngle(float angle) {
-        FishingRod::angle = angle;
-    }
-    //getter and setter for cast
-    bool isCast() const {
-        return cast;
-    }
-    void setCast(bool cast) {
-        FishingRod::cast = cast;
-    }
-    //getter and setter for row
-    bool isRow() const {
-        return row;
-    }
-    void setRow(bool row) {
-        FishingRod::row = row;
+        Entity::angle = angle;
     }
     //getter for name
     const std::string &getName() const {
@@ -176,16 +161,70 @@ public:
     }
 
 private:
-    std::string name;
+    std::string name; //name of entity
     float x; //x location
     float y; //y location
     float dx; // x movement
     float dy; // y movement
     int R; //radius/hitbox
     float angle; //direction of boat/fishing rod
-    bool cast; //catching or not
-    bool row; //moving or not
 };
+//Fishing poll from entity class
+class FishingPoll: Entity
+{
+public:
+    //constructor
+    FishingPoll() {
+        setCast(false);
+        setRow(false);
+    }
+    //getter and setter for cast
+    bool isCast() const {
+        return cast;
+    }
+    void setCast(bool cast) {
+        FishingPoll::cast = cast;
+    }
+    //getter and setter for row
+    bool isRow() const {
+        return row;
+    }
+    void setRow(bool row) {
+        FishingPoll::row = row;
+    }
+private:
+    bool cast;
+    bool row;
+};
+//Fish base class derived from entity class
+class Fish : Entity
+{
+public:
+    //constructor
+    Fish(){
+        FishCount++;
+    }
+    //destructor
+
+    //getter and setter for catch
+    bool isCatch() const {
+        return Catch;
+    }
+    void setCatch(bool Catch) {
+        Fish::Catch = Catch;
+    }
+    //getter for fish count prototype
+    static int getFishCount();
+private:
+    bool Catch;
+    static int FishCount;
+};
+
+int Fish::FishCount{0};
+//getter for FishCount
+int Fish::getFishCount() {
+    return FishCount;
+}
 
 
 int main()
